8ce6c523020de4894f350e7272fbdcaf9e57ba44
Ozkan <tatar66@users.noreply.github.com>
2020-10-31 23:41:25 +0100

    facebook login and find group

--- src/test/java/pages/Facebook_Page.java
+++ src/test/java/pages/Facebook_Page.java
@@ -1,4 +1,37 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Facebook_Page {
+
+    public Facebook_Page() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//button[@title='Accept All']")
+    public WebElement cookiesAcceptedButton;
+
+    @FindBy(id = "email")
+    public WebElement emailTextbox;
+
+    @FindBy(id = "pass")
+    public WebElement passwordTextbox;
+
+    @FindBy(xpath = "//button[@name='login']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "(//span[.='Groups'])[2]")
+    public WebElement groupsMenuLink;
+
+    @FindBy(xpath = "//span[contains(text(),'Promotions')]")
+    public WebElement promotionsGroupMenuLink;
+
+
+
+
+
+
 }
